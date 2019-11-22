11080df462f389b80976c530f2ffb53add2d34d6
Hoko Le <hokole527@gmail.com>
2019-11-22 16:06:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+
+def line(katz_deli)
+  
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else 
+    counter = 1
+    string = "The line is currently:"
+    katz_deli.each do |name| 
+      string << " #{counter}. #{name}" 
+      counter += 1
+    end  
+    
+    puts string
+      
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  
+  welcome = "Welcome, "
+  counter = 2
+  if katz_deli.size == 0
+    puts "Welcome, #{name}. You are number 1 in line." 
+  else
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    counter += 1
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    katz_deli.each do |name|
+      puts "Currently serving #{name}."
+    end
+    katz_deli.shift
+  end
+end  

4f54793ff2d27ad1c0dc5d67e0abb570d3bccba5
Hoko Le <hokole527@gmail.com>
2019-11-22 16:28:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,7 +29,7 @@ def take_a_number(katz_deli, name)
   end
 end
 
-def now_serving(katz_deli)
+def now_serving(katz_deli, name)
   if katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
   else 
